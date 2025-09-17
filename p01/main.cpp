bb4bc9943bbdfbf501817ca542455bf95255be51
Carl <cadalebout1@ccis.edu>
2025-09-17 08:51:22 -0500

    Added std::ostream operator and operator[] to header file

--- p01/main.cpp
+++ p01/main.cpp
@@ -1,19 +1,13 @@
 //main.cpp
 
 #include <iostream>
-#include "FileHandler.h"
+#include "FileManagment.h"
 
 int main ()
 {
-  FileHandler rooms;
+  FileManagment rooms;
   rooms.load_file("rooms.txt");
   std::cout << "finished parse_line \n";
-  for(int i = 0; i < c._info.size(); ++i)
-  {  
-    for(int j = 0; j < c._info[i].size(); ++j)
-    {
-      std::cout << "room: " << i << " item: " << j << " text: " << c._info[i][j] << '\n';
-    }
-  }
+  std::cout << rooms;
   return 0;
 }
